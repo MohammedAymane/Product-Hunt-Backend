7a574f082c89736ee2161b5ff472faf115f044b2
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2023-07-03 22:58:43 +0200

    add doc

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ const express = require("express");
 const cors = require("cors");
 
 const corsOption = {
-  origin: "http://localhost:4200",
+  origin: "http://localhost:4200", // L'origine autorisée pour les requêtes CORS
 };
 
 const app = express();
 
-// Init Midleware
-app.use(cors(corsOption));
-app.use(express.json({ extended: false }));
+// Initialisation des middlewares
+app.use(cors(corsOption)); // Utilisation du middleware CORS pour gérer les requêtes cross-origin
+app.use(express.json({ extended: false })); // Utilisation du middleware express.json() pour analyser les corps de requête JSON
 
+// Endpoint racine pour vérifier si l'API fonctionne
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
 // Define Routes
-app.use("/products", require("./routes/products"));
+app.use("/products", require("./routes/products")); // Utilisation des routes définies dans le fichier "./routes/products"
 
 const PORT = process.env.PORT || 9000;
 
+// Démarrage du serveur sur le port spécifié
 app.listen(PORT, () => {
   console.log("Server started on port : " + PORT);
 });
--- routes/products.js
+++ routes/products.js
@@ -11,6 +11,11 @@ const headers = {
   Authorization: `Bearer ${process.env.API_KEY}`,
 };
 
+/**
+ * Endpoint pour obtenir tous les posts.
+ * Méthode : GET
+ * URL : /api/posts
+ */
 router.get("/", async (req, res) => {
   const query = `{
     posts{
@@ -39,7 +44,7 @@ router.get("/", async (req, res) => {
 
   try {
     const response = await axios.post(url, { query }, { headers });
-    // replace thumbnail object with url string
+    // Remplace l'objet thumbnail par une chaîne de caractères url et l'objet topics par un tableau de noms de sujets
     const responseData = response.data.data.posts.edges.map((edge) => {
       return {
         ...edge.node,
@@ -50,10 +55,17 @@ router.get("/", async (req, res) => {
     return res.json(responseData);
   } catch (error) {
     console.error(error);
-    return res.status(500).send("Internal Server Error");
+    return res.status(500).send("Erreur interne du serveur");
   }
 });
 
+/**
+ * Endpoint pour obtenir les posts par date.
+ * Méthode : GET
+ * URL : /api/posts/date/:date
+ * Paramètres :
+ *   - date: Date au format YYYY-MM-DD
+ */
 router.get("/date/:date", async (req, res) => {
   const date = req.params.date;
   console.log(date);
@@ -84,9 +96,7 @@ router.get("/date/:date", async (req, res) => {
 
   try {
     const response = await axios.post(url, { query }, { headers });
-    // filter by date and replace thumbnail object with url string and topics object with array of topic names
-
-    console.log(response);
+    // Filtre les posts par date, remplace l'objet thumbnail par une chaîne de caractères url et l'objet topics par un tableau de noms de sujets
     const responseData = response.data.data.posts.edges
       .filter((edge) => edge.node.createdAt.includes(date))
       .map((edge) => {
@@ -97,15 +107,6 @@ router.get("/date/:date", async (req, res) => {
         };
       });
 
-    // const responseData = response.data.data.posts.edges
-    //   .filter((edge) => edge.node.createdAt.includes(date))
-    //   .map((edge) => {
-    //     return {
-    //       ...edge.node,
-    //       image: edge.node.thumbnail.url,
-    //     };
-    //   });
-
     return res.json(responseData);
   } catch (error) {
     console.error(error);
@@ -114,65 +115,7 @@ router.get("/date/:date", async (req, res) => {
 });
 
 // router.get("/:id", async (req, res) => {
-//   let page = req.query.page - 1;
-//   if (!req.query.page) {
-//     page = 0;
-//   }
-//   let limit = req.query.limit;
-//   if (!limit) {
-//     limit = 10;
-//   }
-//   let filter = req.query.filter;
-//   let sortby = req.query.sortby;
-//   var newsById;
-
-//   if (req.params.id == "first") {
-//     try {
-//       var news;
-//       if (filter) {
-//         news = await News.find({ field: filter }).sort(sortby);
-//         news = news.slice(page * 10, page * 10 + limit);
-//       } else {
-//         news = await News.find().sort(sortby);
-//         news = news.slice(page * 10, page * 10 + limit);
-//       }
-//       res.send(news[0]);
-//     } catch (err) {
-//       res.status(500).send("Error getting the first news : " + err);
-//       console.log("Error getting the first news : " + err);
-//     }
-//   } else if (req.params.id == "last") {
-//     try {
-//       var news;
-//       if (filter) {
-//         news = await News.find({ field: filter }).sort(sortby);
-//         news = news.slice(page * 10, page * 10 + limit);
-//       } else {
-//         news = await News.find().sort(sortby);
-//         news = news.slice(page * 10, page * 10 + limit);
-//       }
-//       res.send(news[news.length - 1]);
-//     } catch (err) {
-//       res.status(500).send("Error getting the last news : " + err);
-//       console.log("Error getting the last news : " + err);
-//     }
-//   } else if (req.params.id == "count") {
-//     try {
-//       const news = await News.find();
-//       res.send({ count: news.length });
-//     } catch (err) {
-//       res.status(500).send("Error getting the number of news : " + err);
-//       console.log("Error getting the number of news :" + err);
-//     }
-//   } else {
-//     try {
-//       newsById = await News.findOne({ _id: req.params.id });
-//       res.send(newsById);
-//     } catch (err) {
-//       console.log("Error while getting the news by id : " + err);
-//       res.send("Error while getting the news by id : " + err);
-//     }
-//   }
+//   // ...
 // });
 
 module.exports = router;
